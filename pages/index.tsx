001a5a8f770807e2c62965667ae7af6d0fca0c3d
Ralph Largo <devlargs@gmail.com>
2020-09-01 18:09:20 +0800

    remove ralphlargo.com below

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,8 @@ const Index = () => (
   <div className="grid place-items-center h-screen bg-primary">
     <div>
       <h1 className="text-center text-5xl text-gray-300 capitalize">
-        Website maintenance
+        Website under maintenance
       </h1>
-      <h2 className="text-center text-3xl text-red-500">ralphlargo.com</h2>
     </div>
   </div>
 );
