fd9e1c7fa2fca831a908be025be5f6a70359faf7
Ralph Largo <devlargs@gmail.com>
2020-09-01 16:04:31 +0800

    setup nextjs app with tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,8 @@
-export default function Home() {
-  return <div>Coming soon...</div>;
-}
+const Index = () => (
+  <div className="p-4 shadow rounded bg-white">
+    <h1 className="text-purple-500 leading-normal">Next.js</h1>
+    <p className="text-gray-500 flex-">with Tailwind CSS</p>
+  </div>
+);
+
+export default Index;
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  purge: [],
+
+  variants: {},
+  plugins: [],
+  theme: {},
+  variants: {},
+  plugins: [],
+};
