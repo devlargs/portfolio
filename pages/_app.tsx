843c1aa6f556212f603112f2852bc9b0dc7648c6
Ralph Largo <devlargs@gmail.com>
2021-01-24 05:12:56 +0800

    feat:  added projects page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,17 +5,14 @@ import Header from "components/Header";
 function MyApp({ Component, pageProps }) {
   // TODO - Uncomment this when maintenance is done
   return (
-    <>
+    <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
       <link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700,900&display=swap" rel="stylesheet" />
       <main className="bg-white font-montserrat">
         <Header />
       <Component {...pageProps} />
-          
       </main>
-
-
       <Footer />
-    </>
+    </div>
   );
 
 }
