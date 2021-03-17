7b09c895a71aaa36ca7aa81164864a8122c00db5
Ralph Largo <devlargs@gmail.com>
2021-03-18 01:47:48 +0800

    feat: added nprogress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,29 @@
+import NProgress from "nprogress";
 import "styles/tailwind.css";
 import Footer from "components/Footer";
 import Header from "components/Header";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const nprogressStart = () => NProgress.start();
+    const nprogressDone = () => {
+      NProgress.done();
+    };
+    router.events.on("routeChangeStart", nprogressStart);
+    router.events.on("routeChangeComplete", nprogressDone);
+    router.events.on("routeChangeError", nprogressDone);
+
+    return () => {
+      router.events.off("routeChangeStart", nprogressStart);
+      router.events.off("routeChangeComplete", nprogressDone);
+      router.events.off("routeChangeError", nprogressDone);
+    };
+  }, []);
+
   return (
     <div className="overflow-x-hidden">
       <link
