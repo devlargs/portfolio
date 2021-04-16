208aaf18a4802743b9c7a90919f69f01f5628ade
Ralph Largo <devlargs@gmail.com>
2021-04-16 20:41:19 +0800

    chakra sample

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,65 +1,42 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { Button } from "@chakra-ui/button";
+import { Box, Container, Text } from "@chakra-ui/layout";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <Container mx="10" mt="10">
+      <Box>
+        <Button bg="red.100" color="white" mt="1">
+          Button red 100
+        </Button>
+      </Box>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <Box>
+        <Button bg="red.300" color="white" mt="1">
+          Button red 300
+        </Button>
+      </Box>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+      <Box>
+        <Button bg="red.500" color="white" mt="1">
+          Button red 500
+        </Button>
+      </Box>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+      <Box>
+        <Button bg="red.700" color="white" mt="1">
+          Button red 700
+        </Button>
+      </Box>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+      <Box>
+        <Button bg="red.900" color="white" mt="1">
+          Button red 900
+        </Button>
+      </Box>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+      <Text mt="10" fontSize="20" fontWeight="bold">
+        Hello World Chakra and NextJS with Typescript!
+      </Text>
+    </Container>
+  );
 }
