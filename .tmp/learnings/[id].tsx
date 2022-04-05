8a4a4e4e3087411324d7af379fd3ed23e19bb80c
Ralph Largo <devlargs@gmail.com>
2022-04-05 23:15:55 +0800

    feat: added learnings as a temp

---
+++ .tmp/learnings/[id].tsx
@@ -0,0 +1,73 @@
+import { Box, Heading } from '@chakra-ui/react';
+import Container from 'components/Container';
+import NOTION_API_URL from 'constants/notionApiUrl';
+import client from 'helpers/sanityClient';
+import { GetStaticPaths } from 'next';
+import Image from 'next/image';
+import 'prismjs/themes/prism-tomorrow.css';
+import { BLOG_IDS } from 'queries/blogs.query';
+import { LEARNINGS_BY_ID } from 'queries/learnings.query';
+import { NotionRenderer } from 'react-notion';
+import 'react-notion/src/styles.css';
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const types = await client.fetch(BLOG_IDS);
+
+  return {
+    paths: types.map(({ notionId }) => ({
+      params: { id: notionId },
+    })), //indicates that no page needs be created at build time
+    fallback: 'blocking', //indicates the type of fallback
+  };
+};
+
+export async function getStaticProps({ params }) {
+  const data = await fetch(`${NOTION_API_URL}${params.id}`).then((res) => res.json());
+
+  const details = await client.fetch(LEARNINGS_BY_ID, {
+    notionId: params.id,
+  });
+
+  return {
+    props: {
+      blockMap: data,
+      name: details?.[0]?.name ?? '',
+      banner:
+        details?.[0]?.bannerUrl ??
+        'https://images.unsplash.com/photo-1608597003542-9d831d8d6ff4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
+    },
+  };
+}
+
+const Slug = ({ blockMap, name, banner }) => {
+  return (
+    <>
+      <Box w="100vw" pos="relative">
+        <Image
+          layout="responsive"
+          width={2000}
+          height={400}
+          src={banner}
+          objectFit="cover"
+          objectPosition="center center"
+          quality={75}
+          priority
+          // TODO - this is just a temporary fix
+          blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAZAAAADICAQAAABsHH9pAAABgklEQVR42u3TAREAAAQEMN9BX9UlUMBtHZaeAg4RBAQBQUAQEAQEAUFAEBAEBAEEAUFAEBAEBAFBQBAQBAQBQQBBQBAQBAQBQUAQEAQEAUEAQUAQEAQEAUFAEBAEBAFBQBBAEBAEBAFBQBAQBAQBQUAQQBAQBAQBQUAQEAQEAUFAEBAEEAQEAUFAEBAEBAFBQBAQBBAEBAFBQBAQBAQBQUAQEAQEAQQBQUAQEAQEAUFAEBAEBAFBBAFBQBAQBAQBQUAQEAQEAUEAQUAQEAQEAUFAEBAEBAFBQBBAEBAEBAFBQBAQBAQBQUAQQBAQBAQBQUAQEAQEAUFAEBAEEAQEAUFAEBAEBAFBQBAQBBAEBAFBQBAQBAQBQUAQEAQEAQQBQUAQEAQEAUFAEBAEBAEEAUFAEBAEBAFBQBAQBAQBQQBBQBAQBAQBQUAQEAQEAUFAEEFAEBAEBAFBQBAQBAQBQUAQQBAQBAQBQUAQEAQEAUFAEBAEEAQEAUFAEBAEBAFB4KMFzPhzof+L0EYAAAAASUVORK5CYII="
+          placeholder="blur"
+        />
+      </Box>
+
+      <Container minH="calc(100vh - 160px)">
+        <Box style={{ maxWidth: 900 }} margin="auto" p="10" bg="white">
+          <Heading as="h1" mb="10" borderBottom="1px dotted lightgray" p="4">
+            {name}
+          </Heading>
+          <NotionRenderer blockMap={blockMap} />
+        </Box>
+      </Container>
+    </>
+  );
+};
+
+export default Slug;
---
+++ .tmp/learnings/index.tsx
@@ -0,0 +1,51 @@
+import { Box } from '@chakra-ui/react';
+import BlogPosts from 'components/BlogPosts';
+import Container from 'components/Container';
+import Featured from 'components/Featured';
+import LatestPosts from 'components/LatestPosts';
+import client from 'helpers/sanityClient';
+import { LEARNINGS } from 'queries/learnings.query';
+import React from 'react';
+import { globalStyles } from 'styles/global';
+
+const TIL = ({ data }) => {
+  return (
+    <Container minH="calc(100vh - 160px)">
+      <Box mt={globalStyles.container.marginTop} p="5">
+        <Box
+          d={{
+            base: 'inherit',
+            lg: 'flex',
+          }}
+        >
+          <Box flex="1">
+            <Featured />
+          </Box>
+          <Box
+            width={{
+              lg: '400px',
+            }}
+          >
+            <LatestPosts data={data.slice(0, 4)} />
+          </Box>
+        </Box>
+      </Box>
+      <Box mt={globalStyles.container.marginTop}>
+        <BlogPosts data={data} />
+      </Box>
+    </Container>
+  );
+};
+
+export const getStaticProps = async () => {
+  const data = await client.fetch(LEARNINGS);
+
+  return {
+    props: {
+      data,
+    },
+    revalidate: 30,
+  };
+};
+
+export default TIL;
