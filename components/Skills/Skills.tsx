4327cc6d5c124884c27af4ee3389d1ae14334608
Ralph Largo <devlargs@gmail.com>
2023-04-09 14:04:59 +0800

    feat: added skills set

---
+++ components/Skills/Skills.tsx
@@ -0,0 +1,44 @@
+import { Box, Grid, Text } from '@chakra-ui/react';
+import Image from 'next/image';
+import { FC } from 'react';
+
+const Skills: FC<{
+  skills: string[];
+  title: string;
+}> = ({ skills, title }) => {
+  return (
+    <>
+      <Text mb="16px" fontSize="14px" fontWeight={300} color="#c4cfde" letterSpacing="3px" textTransform="uppercase">
+        {title}
+      </Text>
+
+      <Grid gridTemplateColumns="repeat(5, 1fr)" gridGap="16px">
+        {skills.map((item) => {
+          return (
+            <Box
+              key={item}
+              boxShadow="10px 10px 19px #1c1e22, -10px -10px 19px #262a2e"
+              w="65px"
+              h="65px"
+              borderRadius="10px"
+              display="grid"
+              placeContent="center"
+            >
+              <Box w="40px" h="40px" display="grid" placeContent="center">
+                <Image
+                  src={`/images/${item.replace(/ /gi, '-').toLowerCase()}.png`}
+                  alt="React"
+                  style={{ objectFit: 'cover', objectPosition: 'center' }}
+                  width={348}
+                  height={348}
+                />
+              </Box>
+            </Box>
+          );
+        })}
+      </Grid>
+    </>
+  );
+};
+
+export default Skills;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,8 @@
 import { Box, chakra, Text } from '@chakra-ui/react';
 import ContentContainer from '@components/ContentContainer';
+import Skills from '@components/Skills';
+import { PRIMARY_SKILLS, SECONDARY_SKILLS } from 'constants/skills';
+import Image from 'next/image';
 import { FC } from 'react';
 import defaults from 'theme/defaults';
 
@@ -14,6 +17,15 @@ const Home: FC = () => (
     <Box flex="1" color="white">
       <Box display="grid" placeContent="center" h="100vh" p="80px">
         <Box>
+          <Box h="180px" w="180px" mb="20px">
+            <Image
+              src="/images/ralph.jpg"
+              width={3968}
+              height={4016}
+              alt="Logo"
+              style={{ borderRadius: '50%', border: `5px solid #32363b` }}
+            />
+          </Box>
           <Text fontSize="60px" lineHeight="62px" mb="8px" fontWeight="bold" as="h1">
             Hi, I'm <chakra.span color={defaults.primary}>Ralph Largo</chakra.span>
           </Text>
@@ -56,7 +68,15 @@ const Home: FC = () => (
           </Text>
         </ContentContainer>
 
-        <ContentContainer title="About My Skill">
+        <ContentContainer title="Skills">
+          <Skills skills={PRIMARY_SKILLS} title="Primary Skills" />
+          <Box h="16px" />
+          <Skills skills={SECONDARY_SKILLS} title="Secondary Skills" />
+        </ContentContainer>
+
+        <ContentContainer title="Portfolio"></ContentContainer>
+
+        <ContentContainer title="Testimonial">
           <Text fontSize="18px" color="#878e99" mb="25px">
             I’m a developer who has passion for building clean web applications with intuitive functionality. I enjoy
             the process of turning ideas into reality using creative solutions. I mainly use ReactJS. I’m always curious
@@ -68,43 +88,7 @@ const Home: FC = () => (
           </Text>
         </ContentContainer>
 
-        <ContentContainer title="About Me">
-          <Text fontSize="18px" color="#878e99" mb="25px">
-            I’m a developer who has passion for building clean web applications with intuitive functionality. I enjoy
-            the process of turning ideas into reality using creative solutions. I mainly use ReactJS. I’m always curious
-            about learning new skills, tools, and concepts.
-          </Text>
-          <Text fontSize="18px" color="#878e99">
-            In addition to working on various solo hobby projects, I have worked with creative teams, which involves
-            daily stand-ups and communications, source control, and project management.
-          </Text>
-        </ContentContainer>
-
-        <ContentContainer title="About Me">
-          <Text fontSize="18px" color="#878e99" mb="25px">
-            I’m a developer who has passion for building clean web applications with intuitive functionality. I enjoy
-            the process of turning ideas into reality using creative solutions. I mainly use ReactJS. I’m always curious
-            about learning new skills, tools, and concepts.
-          </Text>
-          <Text fontSize="18px" color="#878e99">
-            In addition to working on various solo hobby projects, I have worked with creative teams, which involves
-            daily stand-ups and communications, source control, and project management.
-          </Text>
-        </ContentContainer>
-
-        <ContentContainer title="About Me">
-          <Text fontSize="18px" color="#878e99" mb="25px">
-            I’m a developer who has passion for building clean web applications with intuitive functionality. I enjoy
-            the process of turning ideas into reality using creative solutions. I mainly use ReactJS. I’m always curious
-            about learning new skills, tools, and concepts.
-          </Text>
-          <Text fontSize="18px" color="#878e99">
-            In addition to working on various solo hobby projects, I have worked with creative teams, which involves
-            daily stand-ups and communications, source control, and project management.
-          </Text>
-        </ContentContainer>
-
-        <ContentContainer title="About Me">
+        <ContentContainer title="Contact With Me">
           <Text fontSize="18px" color="#878e99" mb="25px">
             I’m a developer who has passion for building clean web applications with intuitive functionality. I enjoy
             the process of turning ideas into reality using creative solutions. I mainly use ReactJS. I’m always curious
