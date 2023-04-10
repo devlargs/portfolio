4428ecad2faefb9239042343520bf267b562465f
Ralph Largo <devlargs@gmail.com>
2023-04-10 21:48:30 +0800

    feat: added seo

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,79 +7,94 @@ import Skills from '@components/Skills';
 import Testimonials from '@components/Testimonials';
 import { PRIMARY_SKILLS, SECONDARY_SKILLS } from 'constants/skills';
 import { GetStaticProps } from 'next';
+import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { getPlaiceholder } from 'plaiceholder';
 import { FC } from 'react';
+
+const description =
+  'Passionate developer experienced in building clean and intuitive web applications with ReactJS, dedicated to constantly expanding skills and collaborating effectively with creative teams.';
 
 const Home: FC<{ imagePlaceholders: Record<string, string> }> = ({ imagePlaceholders }) => {
   const router = useRouter();
   const isPortfolio = Boolean(router.query.portfolio);
 
   return (
-    <Box
-      display="flex"
-      flexDir={{
-        base: 'column',
-        lg: 'row',
-      }}
-    >
-      <Box flex="1" color="white">
-        <HeroSection logoPlaceholder={imagePlaceholders.ralph} />
-      </Box>
-      <Box flex="1">
-        <Box
-          h={{
-            base: 'initial',
-            lg: '100vh',
-          }}
-          overflow={{
-            base: 'auto',
-            lg: 'scroll',
-          }}
-          overflowX={{
-            base: 'auto',
-            lg: 'hidden',
-          }}
-          py="60px"
-          px={{
-            base: '16px',
-            lg: '100px',
-          }}
-        >
-          <ContentContainer title="About Me">
-            <Text fontSize="18px" color="#878e99" mb="25px">
-              I’m a developer who has passion for building clean web applications with intuitive functionality. I enjoy
-              the process of turning ideas into reality using creative solutions. I mainly use ReactJS. I’m always
-              curious about learning new skills, tools, and concepts.
-            </Text>
-            <Text fontSize="18px" color="#878e99">
-              In addition to working on various solo hobby projects, I have worked with creative teams, which involves
-              daily stand-ups and communications, source control, and project management.
-            </Text>
-          </ContentContainer>
-
-          <ContentContainer title="Skills">
-            <Skills skills={PRIMARY_SKILLS} title="Primary Skills" imagePlaceholders={imagePlaceholders} />
-            <Box h="16px" />
-            <Skills skills={SECONDARY_SKILLS} title="Secondary Skills" imagePlaceholders={imagePlaceholders} />
-          </ContentContainer>
-
-          {isPortfolio && (
-            <ContentContainer title="Portfolio">
-              <Portfolio />
+    <>
+      <Head>
+        <title>Ralph Largo | Portfolio Website</title>
+        <meta name="description" content={description} />
+        <meta property="og:title" content={description} />
+        <meta property="og:description" content={description} />
+        <meta property="og:url" content="https://ralphlargo.com/" />
+        <meta property="og:type" content="website" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
+      <Box
+        display="flex"
+        flexDir={{
+          base: 'column',
+          lg: 'row',
+        }}
+      >
+        <Box flex="1" color="white">
+          <HeroSection logoPlaceholder={imagePlaceholders.ralph} />
+        </Box>
+        <Box flex="1">
+          <Box
+            h={{
+              base: 'initial',
+              lg: '100vh',
+            }}
+            overflow={{
+              base: 'auto',
+              lg: 'scroll',
+            }}
+            overflowX={{
+              base: 'auto',
+              lg: 'hidden',
+            }}
+            py="60px"
+            px={{
+              base: '16px',
+              lg: '100px',
+            }}
+          >
+            <ContentContainer title="About Me">
+              <Text fontSize="18px" color="#878e99" mb="25px">
+                I’m a developer who has passion for building clean web applications with intuitive functionality. I
+                enjoy the process of turning ideas into reality using creative solutions. I mainly use ReactJS. I’m
+                always curious about learning new skills, tools, and concepts.
+              </Text>
+              <Text fontSize="18px" color="#878e99">
+                In addition to working on various solo hobby projects, I have worked with creative teams, which involves
+                daily stand-ups and communications, source control, and project management.
+              </Text>
             </ContentContainer>
-          )}
 
-          <ContentContainer title="Recommendations">
-            <Testimonials imagePlaceholders={imagePlaceholders} />
-          </ContentContainer>
+            <ContentContainer title="Skills">
+              <Skills skills={PRIMARY_SKILLS} title="Primary Skills" imagePlaceholders={imagePlaceholders} />
+              <Box h="16px" />
+              <Skills skills={SECONDARY_SKILLS} title="Secondary Skills" imagePlaceholders={imagePlaceholders} />
+            </ContentContainer>
 
-          <ContentContainer title="Contact With Me">
-            <ContactForm />
-          </ContentContainer>
+            {isPortfolio && (
+              <ContentContainer title="Portfolio">
+                <Portfolio />
+              </ContentContainer>
+            )}
+
+            <ContentContainer title="Recommendations">
+              <Testimonials imagePlaceholders={imagePlaceholders} />
+            </ContentContainer>
+
+            <ContentContainer title="Contact With Me">
+              <ContactForm />
+            </ContentContainer>
+          </Box>
         </Box>
       </Box>
-    </Box>
+    </>
   );
 };
 
